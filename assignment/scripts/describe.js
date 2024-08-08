7cee5805597729402caf29b091eb8bfcba249ab6
Shobha Chejara <shobha.chejara@gmail.com>
2024-08-08 11:17:43 -0500

    Updates in describe.js file

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -24,15 +24,11 @@ if (number >= 2) {
 
 */
 
+//1. WRITE YOUR DESCRIPTION HERE
 
-// 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-//
-//
-//
-//
-//
+// We create a variable called name and set it to 'Dane' as a String.
+// Now name is equal to 'Dane'.
+// Then we check if name is equal to 'Mary', Dane is not equal to Mary. so we console.log 'How do you do'.
 
 //CODE
 /*
@@ -48,13 +44,16 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+//    We create a variable called secret.
+//    We create a another number variable called code and set it to 123.
+//    Then we check if code is equal to 123
+//    As code is equal to 123 then secret is equal to 'super'. 
+//    And then code is multiplied by 2, now code value is 246.
+//    So we console.log value of secret variable which is 'super'.
 //
-
 //CODE
 /*
+
 let secret;
 
 let code = 123;
@@ -72,11 +71,15 @@ console.log(secret)
 
 */
 
-// 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//3.  We create a variable called isStudent and set it to 'true' as a boolean.
+//    We create a second variable called age and set it to 34 as a number.
+//    We create a third variable called zip and set it to 55407 as a number.
+//    Then we check if isStudent value is equal to true and zip value is greater than 80000 .
+//    As isStudent value is equal to true and zip value is not greater than 80000.
+//    Then we check second conditon if isStudent value is equal to false or age is less than 30.
+//    As isStudent is not equal to false and age is not less than 30.
+//    Then we check third condition if isStudent value is equal to true.
+//    As isStudent value is equal to true. So we console.log ''Welcome to Prime!'.
 
 //CODE
 /*
@@ -97,8 +100,6 @@ if (isStudent === true && zip > 80000 ) {
 */
 
 
-
-
 // REVERSE
 // Now, for questions 4-6, we'll give you a description and some code. 
 // Some of the code will NOT match the description.
@@ -116,7 +117,7 @@ if (isStudent === true && zip > 80000 ) {
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number -- decrements number, but the instructions ask to increment. 
 // Should be number++.
 number--; 
 
@@ -133,22 +134,52 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+//FIX - colorOne is set to red but the instructions asks to set it to blue.
+//Should be let colorOne = 'blue'.
 let colorOne = 'red';
-let colorTwo = 'blue';
-let mix = true;
 
-if (mix === true) {
+//FIX -  colorTwo is set to red but the instructions asks to set it to red.
+
+//Should be  let colorTwo = 'red'.
+
+    let colorTwo = 'blue';
+
+    let mix = true;
+
+    if (mix === true) {
+
+//FIX - Mix value is True but only ColorOne value is set to 'purple'
+        but instructions asks to set both colorOne and colorTwo value as purple
+        Added missing statement to add colorTwo value as purple
+ 
+
+//Add missing statement colorTwo = 'purple';
+
   colorOne = 'purple';
 }
 */
 
+
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+//    We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
 /*
+
 let temp = 40;
+
+FIX - Variable time is declared as const, but since time can change then
+      time variable should also be declared with let
+      
+Should be  let time = 4; 
 const time = 4;
+
+FIX - Condition states that if either temp is greater than 39 or time is greater and
+      equal than 4 then cosoloe log the message.
+//    but the  instructions asks to check if both the conditions are 'true'
+      then only console log the message 'throw away the food!'.
+
+//Should be - if(temp > 39 && time >= 4 )
 
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
@@ -160,13 +191,30 @@ if (temp > 39 || time >= 4) {
 // -- it is, so we console.log 'enter'.
 
 /*
-let age = 21;
-const minAge = 21;
+   let age = 21;
+   const minAge = 21;
 
-if(minAge <= age) {
-  console.log('no entry');
-} else {
-  console.log('enter');
+FIX- Condition mentioned is if minAge is less than or equal to 
+     but as per instructions DESCRIPTION age should be greater than or equal to minAge    
+
+    //should be if(age >= minAge){
+
+     if(minAge <= age) {
+
+FIX- As per instructions if age is greater than or equal to minAge console log message 'enter'
+     but in the code it is specified to console log message 'no entry' which was not instructed. 
+
+   //Should be console.log('enter')
+
+      console.log('no entry');
+      } else {
+
+FIX- As per instructions if age is not greater than or equal to minAge consloe log message 'no entry' 
+     but in the code it is specified to console log message'enter'
+
+//  should be console.log('no entry')
+
+     console.log('enter');
 }
 */
 
